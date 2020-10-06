734f4f4f350b40e8ee700f4d5f682c1dec64f2d5
pardf <azureuser@ubuntuTest.epx03rbpjt2uter1v0vz1j4qme.zx.internal.cloudapp.net>
2020-10-06 16:22:55 +0100

    Navigation bar links update

--- ai4good/webapp/run_model_page.py
+++ ai4good/webapp/run_model_page.py
@@ -318,8 +318,9 @@ def history_table():
 def nav_bar():
     return dbc.NavbarSimple(
         children=[
-            dbc.NavItem(dbc.NavLink("Home", href="https://www.aiforgood.co.uk/")),
-            dbc.NavItem(dbc.NavLink("About us", href="https://www.aiforgood.co.uk/about-us")),
+            dbc.NavItem(dbc.NavLink("Home", href="https://www.aiforgood.co.uk/", target="_blank")),
+            dbc.NavItem(dbc.NavLink("What is it?", href="https://www.aiforgoodsimulator.com/", target="_blank")),
+            dbc.NavItem(dbc.NavLink("About us", href="https://www.aiforgood.co.uk/about-us", target="_blank")),
         ],
         brand="AI for Good Simulator",
         brand_href="#",
