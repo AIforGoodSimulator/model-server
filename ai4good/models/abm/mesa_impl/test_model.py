d1e3a5972add4ac974d3125d294e5029a5d8a890
Vera Ekimenko <vekimenko@gmail.com>
2020-10-13 00:40:12 +0100

    Fix position_block function reference in the model.

--- ai4good/models/abm/mesa_impl/test_model.py
+++ ai4good/models/abm/mesa_impl/test_model.py
@@ -23,31 +23,35 @@ class MyTestCase(unittest.TestCase):
         params = get_params()
         mod = mm.Camp(params=params)
 
-        # np.savetxt('agents_age.csv', mod.agents_age, delimiter=',', header='agent_age')
-        # np.savetxt('agents_disease_states.csv', mod.agents_disease_states, delimiter=',',
-        #            header='agents_disease_states')
-        # np.savetxt('agents_gender.csv', mod.agents_gender, delimiter=',',
-        #            header='agents_gender')
-        # np.savetxt('agents_home_ranges.csv', mod.agents_home_ranges, delimiter=',',
-        #            header='agents_home_ranges')
-        # np.savetxt('agents_households.csv', mod.agents_households, delimiter=',',
-        #            header='agents_households')
-        # np.savetxt('agents_incubation_periods.csv', mod.agents_incubation_periods, delimiter=',',
-        #            header='agents_incubation_periods')
-        # np.savetxt('agents_pos.csv', mod.agents_pos, delimiter=',',
-        #            header='agents_pos1,agents_pos2')
-        # np.savetxt('agents_route.csv', mod.agents_route, delimiter=',',
-        #            header='agents_route')
-        # print("foodline_queue: " + str(mod.foodline_queue))
-        # np.savetxt('toilets.csv', mod.toilets, delimiter=',',
-        #            header='toilets1,toilets2')
-        # print("toilets_queue: " + str(mod.toilets_queue))
-        #
-        # np.savetxt('foodlines.csv', mod.foodlines, delimiter=',',
-        #            header='foodlines1,foodlines2')
-        # np.savetxt('households.csv', mod.households, delimiter=',',
-        #            header='households1,households2')
+        test = 1
 
+        np.savetxt('agents_age.csv', mod.agents_age, delimiter=',', header='agent_age')
+        np.savetxt('agents_disease_states.csv', mod.agents_disease_states, delimiter=',',
+                   header='agents_disease_states')
+        np.savetxt('agents_gender.csv', mod.agents_gender, delimiter=',',
+                   header='agents_gender')
+        np.savetxt('agents_home_ranges.csv', mod.agents_home_ranges, delimiter=',',
+                   header='agents_home_ranges')
+        np.savetxt('agents_households.csv', mod.agents_households, delimiter=',',
+                   header='agents_households')
+        np.savetxt('agents_incubation_periods.csv', mod.agents_incubation_periods, delimiter=',',
+                   header='agents_incubation_periods')
+        np.savetxt('agents_pos.csv', mod.agents_pos, delimiter=',',
+                   header='agents_pos1,agents_pos2')
+        np.savetxt('agents_route.csv', mod.agents_route, delimiter=',',
+                   header='agents_route')
+        print("foodline_queue: " + str(mod.foodline_queue))
+        np.savetxt('toilets.csv', mod.toilets, delimiter=',',
+                   header='toilets1,toilets2')
+        print("toilets_queue: " + str(mod.toilets_queue))
+
+        np.savetxt('foodlines.csv', mod.foodlines, delimiter=',',
+                   header='foodlines1,foodlines2')
+        np.savetxt('households.csv', mod.households, delimiter=',',
+                   header='households1,households2')
+
+        np.savetxt('agents_ethnic_groups.csv', mod.agents_ethnic_groups, delimiter=',',
+                   header='agents_ethnic_groups')
         # print("people_count: " + str(mod.people_count))
         # print("running: " + str(mod.running))
 
