65b66ebaa86ae1b795679c67d4851a17d7602bc8
pardf <hgpeter@hotmail.com>
2020-10-15 07:50:30 +0100

    update input pages 1,2,3,4

--- ai4good/webapp/input_page_1.py
+++ ai4good/webapp/input_page_1.py
@@ -10,11 +10,26 @@ from ai4good.webapp.model_runner import ModelScheduleRunResult
 from dash.dependencies import Input, Output, State
 import dash_table
 
+text_disclaimer = 'Disclaimer: This tool is for informational and research purposes only and should not be considered as a medical predictor. The input parameters you have provided is a determining factor in the simulation results. '
 
 layout = html.Div(
     [
         run_model_page.nav_bar(),
- 
+        
+        html.Div([
+            dbc.Container([
+                dbc.Row(
+                    dbc.Col(
+                        dbc.Card([
+                            html.H6(text_disclaimer, className='card-text'),
+                            html.Div(id='input-page-1-disclaimer')
+                            ], body=True
+                        ), width=6
+                    ), justify='center', style={'margin-top':'10px'}
+                )
+            ])
+        ]), 
+
         html.Div([
             dbc.Container([
                 dbc.Row(
@@ -33,7 +48,6 @@ layout = html.Div(
                             html.Header('Total Area (sq. km)', className='card-text'),
                             dbc.Input(id='population', placeholder='Optional', type='number', min=0, max=100000, step=100, bs_size='sm', style={'margin-bottom':'25px'}),
                             dbc.CardFooter(dbc.Button('Next', id='page-1-button', color='dark', href='/sim/input_page_2')),
-                            html.Br(),
                             html.Div(id='input-page-1-alert')
                             ], body=True
                         ), width=6
--- ai4good/webapp/input_page_2.py
+++ ai4good/webapp/input_page_2.py
@@ -7,18 +7,22 @@ import dash_html_components as html
 from ai4good.webapp.apps import dash_app, facade, model_runner
 import ai4good.webapp.run_model_page as run_model_page
 from ai4good.webapp.model_runner import ModelScheduleRunResult
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output, State, ALL
 import dash_table
 
 age = ['0 - 5', '6 - 9', '10 - 19', '20 - 29', '30 - 39', '40 - 49', '50 - 59', '60 - 69', '70+']
-id_age_perc =['Age-percentage-' + x.replace(' ','') for x in age]
+id_age_perc =['age-percentage-' + x.replace(' ','') for x in age]
 dist = [10, 10, 10, 10, 10, 10, 10, 10, 20]
 
-def generate_html_age_group(age):
-    return html.Header([str(age) + ' years'], className='card-text')
-
-def generate_input_distribution(id_age_perc, dist):
-    return dbc.Input(id=id_age_perc, placeholder='Required', value=dist, type='number', min=0, max=100, step=1, bs_size='sm', style={'margin-bottom':'25px'})
+def generate_html_age_group(age, id_age_perc, dist):
+    return html.Div([
+               html.Header(''),
+               html.Header([str(age)], className='card-text'), 
+               dbc.Input(id={
+                   'type':'age-dist-input',
+                   'index':id_age_perc
+               }, placeholder='Required', value=dist, type='number', min=0, max=100, step=1, bs_size='sm', style={'justify':'right', 'margin-bottom':'25px'}), 
+           ], style={'display':'grid', 'grid-template-columns':'20% 60% 20%', 'margin-below':'25px'})
 
 layout = html.Div(
     [
@@ -31,29 +35,33 @@ layout = html.Div(
                         dbc.Card([
                             html.H4('COVID-19 Simulator', className='card-title'),
                             html.Center(html.Img(src='/static/placeholder286x180.png', title='Step 2 of 4', style={'width':'50%'})), 
-                            html.P('Enter the percentage of the total population that each range represents (total percentage should be 100)',className='card-text'),
+                            html.P('Enter the percentage of the total population that each range represents',className='card-text'),
                             html.H5('Age Group Distribution (%)', className='card-text'),
-                            generate_html_age_group(age[0]),
-                            generate_input_distribution(id_age_perc[0], dist[0]), 
-                            generate_html_age_group(age[1]),
-                            generate_input_distribution(id_age_perc[1], dist[1]), 
-                            generate_html_age_group(age[2]),
-                            generate_input_distribution(id_age_perc[2], dist[2]), 
-                            generate_html_age_group(age[3]),
-                            generate_input_distribution(id_age_perc[3], dist[3]), 
-                            generate_html_age_group(age[4]),
-                            generate_input_distribution(id_age_perc[4], dist[4]), 
-                            generate_html_age_group(age[5]),
-                            generate_input_distribution(id_age_perc[5], dist[5]), 
-                            generate_html_age_group(age[6]),
-                            generate_input_distribution(id_age_perc[6], dist[6]), 
-                            generate_html_age_group(age[7]),
-                            generate_input_distribution(id_age_perc[7], dist[7]), 
-                            generate_html_age_group(age[8]),
-                            generate_input_distribution(id_age_perc[8], dist[8]), 
-                            html.Br(),
+                            html.Div([
+                                html.H5(''), 
+                                html.H5('Age Group (years)'), 
+                                html.H5('Distribution (%)')], 
+                                style={'display':'grid', 'grid-template-columns':'10% 60% 30%'}),
+                            generate_html_age_group(age[0], id_age_perc[0], dist[0]),
+                            generate_html_age_group(age[1], id_age_perc[1], dist[1]),
+                            generate_html_age_group(age[2], id_age_perc[2], dist[2]),
+                            generate_html_age_group(age[3], id_age_perc[3], dist[3]),
+                            generate_html_age_group(age[4], id_age_perc[4], dist[4]),
+                            generate_html_age_group(age[5], id_age_perc[5], dist[5]),
+                            generate_html_age_group(age[6], id_age_perc[6], dist[6]),
+                            generate_html_age_group(age[7], id_age_perc[7], dist[7]),
+                            generate_html_age_group(age[8], id_age_perc[8], dist[8]),
+                            html.Div([
+                                html.B(''), 
+                                html.B('Total:'), 
+                                html.B('100%', id='age_percentage_total')], 
+                                style={'display':'grid', 'grid-template-columns':'20% 60% 20%', 'margin-below':'25px'}),
+                            html.Div([
+                                html.P(''), 
+                                html.P('Must equal to 100%', className='card-text', style={'color':'darkgray'}), 
+                                html.P('')], 
+                                style={'display':'grid', 'grid-template-columns':'20% 60% 20%', 'margin-below':'25px'}),
                             dbc.CardFooter(dbc.Button('Next', id='page-2-button', color='dark', href='/sim/input_page_3')),
-                            html.Br(),
                             html.Div(id='input-page-2-alert')
                             ], body=True
                         ), width=6
@@ -63,3 +71,9 @@ layout = html.Div(
         ])
     ]
 )
+
+@dash_app.callback(
+    Output('age_percentage_total', 'children'), 
+    [Input({'type':'age-dist-input', 'index':ALL}, 'value')])
+def update_age_range_input_moved_off_site(values):
+    return ([str(sum(values)) + '%'])
