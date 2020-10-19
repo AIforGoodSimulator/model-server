4ec4aa1a9cf6dbe6eae9ad2cfd3df3042ef02f67
Sohail <sohail@helpmewithit.com>
2020-10-19 17:45:54 +0100

    test dask

--- azure_startup.sh
+++ azure_startup.sh
@@ -2,4 +2,5 @@ apt-get install -y gcc
 apt-get install -y build-essential libssl-dev libffi-dev python3.6-dev
 apt-get install -y python-numba python3-dask
 pip install -r requirements.txt
+pip install "dask[distributed]" --upgrade
 gunicorn --bind=0.0.0.0 --timeout 600 --chdir ai4good/webapp server:flask_app
