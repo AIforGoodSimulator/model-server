7de65e3e2024cc938e6f478c247f55b20fc5fb48
Vikram Bajaj <vikrambajaj220496@gmail.com>
2020-10-23 10:54:08 -0400

    add config for message 5

--- ai4good/webapp/model_results_config.py
+++ ai4good/webapp/model_results_config.py
@@ -1,8 +1,14 @@
 model_profile_config = {
     "message_1":
         {
-            'compartmental-model':['baseline','better_hygiene_one_month','better_hygiene_three_month','better_hygiene_six_month'],
-            'agent-based-model':[],
-            'network-model':[],
-        }
+            'compartmental-model': ['baseline', 'better_hygiene_one_month', 'better_hygiene_three_month', 'better_hygiene_six_month'],
+            'agent-based-model': [],
+            'network-model': [],
+        },
+    "message_5":
+    {
+        'compartmental-model': ['only_better_hygiene', 'only_remove_symptomatic', 'only_remove_high_risk', 'combined_hygiene_symptomatic_high_risk'],
+        'agent-based-model': [],
+        'network-model': [],
+            }
 }
