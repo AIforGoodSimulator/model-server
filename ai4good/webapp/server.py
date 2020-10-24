38043203670e1bad1b7be335204a7186a7484c61
Billy Zhao <billyzhaoyh@gmail.com>
2020-10-24 14:57:10 +0100

    Upload the waiting page done by the student team

--- ai4good/webapp/server.py
+++ ai4good/webapp/server.py
@@ -21,6 +21,7 @@ import ai4good.webapp.nm_model_report_page as nm_model_report_page
 import ai4good.webapp.nm_admin_page as nm_admin_page
 import ai4good.webapp.model_results_scaffolding as model_results_scaffolding
 import ai4good.webapp.report_poc_graph as report_poc_graph
+import ai4good.webapp.waiting_page as waiting_page
 from ai4good.webapp.apps import flask_app, dash_app
 
 
@@ -87,7 +88,8 @@ def display_page(pathname, query=None):
         return model_results_scaffolding.layout()
     elif pathname == '/sim/poc_graph_test':
         return report_poc_graph.layout()
-
+    elif pathname == '/sim/waiting':
+        return waiting_page.layout
     else:
         return '404'
 
