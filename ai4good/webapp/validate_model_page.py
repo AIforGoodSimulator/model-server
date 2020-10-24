4cd7dca94e78849548a27e35d53d04dfc9b443cc
Joseph Rance <56409230+Joseph-Rance@users.noreply.github.com>
2020-10-24 20:32:26 +0100

    Add Autocorrelation and Partial Autocorrelation plots
    Added function to calculate autocorrelation and partial autocorrelation and graphs to plot them. I couldn't find any inputs for the function so I used the code for random inputs from the validate_model repo

--- ai4good/webapp/validate_model_page.py
+++ ai4good/webapp/validate_model_page.py
@@ -9,10 +9,55 @@ import ai4good.webapp.run_model_page as run_model_page
 from ai4good.webapp.model_runner import ModelScheduleRunResult
 import ai4good.utils.path_utils as pu
 from dash.dependencies import Input, Output, State
+import plotly.express as px
 import dash_table
 import os
-import datetime
+from datetime import date, timedelta
 import sklearn
+
+def get_autocorrelation_plots(pred):
+    def get_autocorrelation(sequence, shifts=31):
+        correlations = []
+        
+        for shift in range(1, shifts):
+            correlation = np.corrcoef(sequence[:-shift], sequence[shift:])[0, 1]
+            correlations.append(correlation)
+        return [1] + correlations  # correlation with 0 shift -> 1
+
+    def get_partial_autocorrelation(sequence, shifts=31):
+        p_correlations = []
+
+        residuals = sequence
+        for shift in range(1, shifts):
+            correlation = np.corrcoef(sequence[:-shift], residuals[shift:])[0, 1]
+            p_correlations.append(correlation)
+
+            m, c =  np.polyfit(sequence[:-shift], residuals[shift:], 1)  # m -> grad.; c -> intercept
+            residuals[shift:] = residuals[shift:] - (m * sequence[:-shift] + c)
+        return [1] + p_correlations
+
+    ac_df = pd.DataFrame(data={"shift": np.linspace(0, 30, 31), "ac": get_autocorrelation(pred.to_numpy().copy())})
+    pac_df = pd.DataFrame(data={"shift": np.linspace(0, 30, 31), "pac": get_partial_autocorrelation(pred.to_numpy())})
+
+    ac_fig = px.bar(ac_df, x="shift", y="ac", title="autocorrelation")
+    pac_fig = px.bar(pac_df, x="shift", y="pac", title="partial autocorrelation")
+
+    return ac_fig, pac_fig
+
+def get_random_pred():  # Get random pred for graph demo
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    df = pd.DataFrame(columns=["date", "actual", "pred"])
+    today = date.today()
+    begin = today - timedelta(days=99)
+    df["date"] = pd.date_range(begin, today)
+    data = np.random.randint(100,150,size=(100,2))
+    df[["actual", "pred"]] = pd.DataFrame(data, columns=["actual", "pred"])
+    pred = df["pred"]
+    
+    return pred
+
+pred = get_random_pred()
+ac_fig, pac_fig = get_autocorrelation_plots(pred)
 
 layout = html.Div(
     [
@@ -32,6 +77,8 @@ layout = html.Div(
                             html.H5('Section 2', className='card-text'),
                             html.Header('Result 3', className='card-text'),
                             html.Header('Result 4', className='card-text'),
+                            dcc.Graph(figure=ac_fig),
+                            dcc.Graph(figure=pac_fig),
                             dbc.CardFooter(dbc.Button('Back', id='validate-model-button', color='secondary', href='/sim/run_model', style={'float':'right'})),
                             html.Div(id='validate-model-page-alert'), 
                             ], body=True), 
