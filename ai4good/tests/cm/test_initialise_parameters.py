4cd69e795e3e5d962a0bf47f585a459409ba9376
Sohail <sohail@helpmewithit.com>
2020-11-04 00:24:23 +0000

    fix broken test

--- ai4good/tests/cm/test_initialise_parameters.py
+++ ai4good/tests/cm/test_initialise_parameters.py
@@ -37,6 +37,6 @@ class InitialiseParameters(unittest.TestCase):
 
         self.assertEqual(params.control_dict['numberOfIterations'], 2)
         self.assertEqual(params.control_dict['t_sim'], 200)
-        expected = pd.read_csv('./resources/Greece_contact_matrix.csv', index_col=0)
+        expected = pd.read_csv('resources/Greece_contact_matrix.csv', index_col=0)
         actual = pd.DataFrame(params.infection_matrix)
         self.assertTrue(np.allclose(expected.values, actual.values))
