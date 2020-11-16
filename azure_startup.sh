8490e7f421ac74434cdcbb5853ec0ab32580f61c
sohail <sohail@helpmewithit.com>
2020-11-16 12:04:26 +0000

    added sdeint

--- azure_startup.sh
+++ azure_startup.sh
@@ -1,6 +1,6 @@
 apt-get install -y gcc
 apt-get install -y build-essential libssl-dev libffi-dev 
-apt-get install -y python3-blosc
+apt-get install -y python3-blosc xterm zip
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 gunicorn --bind=0.0.0.0 --timeout 600 --chdir ai4good/webapp server:flask_app
