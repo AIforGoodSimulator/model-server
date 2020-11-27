865dbf9dbd98eb5260e6622bda5b8438e56370b4
Billy Zhao <billyzhaoyh@gmail.com>
2020-11-27 22:49:22 +0000

    Update the way user input parameters are read in the tests

---
+++ ai4good/runner/tests/__init__.py
@@ -0,0 +1,30 @@
+# Set a default user input for debugging purposes:
+user_input_params = ' {"name-camp": "Moria", "location": "Greece", "country-dropdown": "Greece", ' \
+                    '"total-area": "null", "total-population": 18700, "age-population-0-5": 1968.175, ' \
+                    '"age-population-6-9": 1968.175, "age-population-10-19": 3242.58, ' \
+                    '"age-population-20-29": 4927.45, "age-population-30-39": 3208.92, ' \
+                    '"age-population-40-49": 1727.88, "age-population-50-59": 1037.85, ' \
+                    '"age-population-60-69": 474.98, "age-population-70+": 143.99, "gender-perc-female": 50,' \
+                    '"gender-perc-male": 50, "ethnic-no-1": "null", "ethnic-no-2": "null", "ethnic-no-3": "null",' \
+                    '"ethnic-no-4": "null", "ethnic-no-5": "null", "ethnic-no-6": "null", "ethnic-no-7": "null",' \
+                    '"ethnic-no-8": "null", "ethnic-no-9": "null", "ethnic-no-10": "null",' \
+                    '"accommodation-area-type1": "null", "accommodation-area-type2": "null",' \
+                    '"accommodation-area-type3": "null", "accommodation-no-unit-type1": "null",' \
+                    '"accommodation-no-unit-type2": "null", "accommodation-no-unit-type3": "null",' \
+                    '"accommodation-no-person-type1": "null", "accommodation-no-person-type2": "null",' \
+                    '"accommodation-no-person-type3": "null", "available-ICU-beds": "null",' \
+                    '"increased-ICU-beds": "null", "remove-high-risk-off-site": "null",' \
+                    '"age-min-moved-off-site": 60, "age-max-moved-off-site": 100,' \
+                    '"number-known-comorbidity": "null", "isolation-centre-capacity": "null",' \
+                    '"days-quarantine-tested-positive": "null", "community-shielding": "null",' \
+                    '"community-surveillance-program": "null", "radio-intervene-social": "null",' \
+                    '"radio-intervene-face": "null", "radio-intervene-handwashing": "null",' \
+                    '"radio-intervene-testing": "null", "radio-intervene-lockdown": "null",' \
+                    '"activity-no-place-admin": "null", "activity-no-place-food": "null",' \
+                    '"activity-no-place-health": "null", "activity-no-place-recreational": "null",' \
+                    '"activity-no-place-religious": "null", "activity-no-person-admin": "null",' \
+                    '"activity-no-person-food": "null", "activity-no-person-health": "null",' \
+                    '"activity-no-person-recreational": "null", "activity-no-person-religious": "null",' \
+                    '"activity-no-visit-admin": "null", "activity-no-visit-food": "null",' \
+                    '"activity-no-visit-health": "null", "activity-no-visit-recreational": "null",' \
+                    '"activity-no-visit-religious": "null"}'
--- ai4good/runner/tests/test_end_to_end_stoch_cm_model_runs.py
+++ ai4good/runner/tests/test_end_to_end_stoch_cm_model_runs.py
@@ -1,31 +1,32 @@
-# import unittest
-# import numpy as np
-# import pandas as pd
-# import pandas.testing as pdt
-# from ai4good.models.cm.cm_model import CompartmentalModelStochastic
-# import ai4good.runner.console_runner as cr
-# import ai4good.utils.path_utils as pu
-#
-#
-# class TestEndToEndRuns(unittest.TestCase):
-#
-#     def test_custom_run(self):
-#         mr = cr.run_model(
-#             _model=CompartmentalModelStochastic.ID,
-#             _profile='custom',
-#             camp='Moria',
-#             load_from_cache=False,
-#             save_to_cache=False,
-#             is_save_plots=False,
-#             is_show_plots=False,
-#             is_save_report=False,
-#             overrides='{"numberOfIterations": 4, "nProcesses": 1, "random_seed": 7}'
-#         )
-#         actual_report_df = mr.get('report')
-#         # actual_report_df.to_csv(pu._path('../runner/tests', 'stoch_cm_expected_report.csv'), index=False) # for making new snapshot
-#         expected_df = pd.read_csv(pu._path('../runner/tests', 'stoch_cm_expected_report.csv'))
-#         pdt.assert_frame_equal(expected_df, actual_report_df, check_exact=False, check_less_precise=0)
-#
-#
-#
-#
+import unittest
+import numpy as np
+import pandas as pd
+import pandas.testing as pdt
+from ai4good.models.cm.cm_model import CompartmentalModelStochastic
+import ai4good.runner.console_runner as cr
+import ai4good.utils.path_utils as pu
+from ai4good.runner.tests import user_input_params
+
+
+class TestEndToEndRuns(unittest.TestCase):
+
+    def test_custom_run(self):
+        mr = cr.run_model(
+            _model=CompartmentalModelStochastic.ID,
+            _profile='custom',
+            user_input_params=user_input_params,
+            load_from_cache=False,
+            save_to_cache=False,
+            is_save_plots=False,
+            is_show_plots=False,
+            is_save_report=False,
+            overrides='{"numberOfIterations": 4, "nProcesses": 1, "random_seed": 7}'
+        )
+        actual_report_df = mr.get('report')
+        # actual_report_df.to_csv(pu._path('../runner/tests', 'stoch_cm_expected_report.csv'), index=False) # for making new snapshot
+        expected_df = pd.read_csv(pu._path('../runner/tests', 'stoch_cm_expected_report.csv'))
+        pdt.assert_frame_equal(expected_df, actual_report_df, check_exact=False, check_less_precise=0)
+
+
+
+
