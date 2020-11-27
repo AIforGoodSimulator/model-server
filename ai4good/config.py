867c469d6110482c800b6d61fa32d147ddde75cb
Peter Hung <hgpeter@hotmail.com>
2020-11-27 05:20:15 +0000

    Updte config.py for better app setup and secrets management

--- ai4good/config.py
+++ ai4good/config.py
@@ -1,11 +1,17 @@
 import os
+import datetime
 
-# class for basic setup and secrets management
-class BaseConfig:
+# class for secrets management
+class FlaskConfig:
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + os.environ.get('DATABASE_USERNAME') + ':' os.environ.get('DATABASE_PASSWORD') + '@' + os.environ.get('DATABASE_URL')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ['SECRET_KEY']
+    REMEMBER_COOKIE_DURATION = datetime.timedelta(minutes=30)
+
+# class for app setup
+class ModelConfig:
     MAX_CONCURRENT_MODELS = 30
     HISTORY_SIZE = 100
     INPUT_PARAMETER_TIMEOUT = 60*30 # in seconds
-    CACHE_TIMEOUT = 60*60*2  # in seconds (not used yet)
+    CACHE_TIMEOUT = 60*60*2  # in seconds
